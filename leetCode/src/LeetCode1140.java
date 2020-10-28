f73aa49a7370d116803590a84b541c0328dfea62
qihaodong1 <qihaodong3@jd.com>
2020-10-28 22:20:32 +0800

    @dev：Stone problem

--- leetCode/src/LeetCode1140.java
+++ leetCode/src/LeetCode1140.java
@@ -4,9 +4,8 @@
 public class LeetCode1140 {
 
     public static void main(String[] args) {
-        LeetCode1140 s = new LeetCode1140();
         int[] a = {1, 2, 3, 4, 5, 100};
-        s.stoneGameII(a);
+        new LeetCode1140().stoneGameII(a);
     }
 
     /**
@@ -16,16 +15,16 @@ public class LeetCode1140 {
      * @return
      */
     public int stoneGameII(int[] piles) {
-        int length = piles.length, temp = 0;
+        int length = piles.length, echo = 0;
         int[][] dp = new int[length + 1][length];
         for (int i = length; i >= 1; i--) {
-            temp += piles[i - 1];
+            echo += piles[i - 1];
             for (int j = 1; j < length; j++) {
-                if (i + j * 2 >= length) {
-                    dp[i][j] = temp;
+                if (i + 2 * j > length) {
+                    dp[i][j] = echo;
                 } else {
-                    for (int z = 1; z <= j * 2; z++) {
-                        dp[i][j] = Math.max(dp[i][j], temp - dp[i + z][Math.max(j, z)]);
+                    for (int t = 1; t <= 2 * j; t++) {
+                        dp[i][j] = Math.max(dp[i][j], echo - dp[i + t][Math.max(t, j)]);
                     }
                 }
             }
