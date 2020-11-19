85e041624ab96f467fc6a062feec423b4867002e
qihaodong0309 <qihaodong0309@163.com>
2020-11-19 11:01:07 +0800

    @dev：LeetCode 283(quick and slow pointer)

---
+++ leetCode/src/dailyrecord/LeetCode283.java
@@ -0,0 +1,48 @@
+package dailyrecord;
+
+/**
+ * @author qihaodong
+ */
+public class LeetCode283 {
+
+    /**
+     * 简单遍历
+     *
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            while (end >= 0 && nums[end] == 0) {
+                end--;
+            }
+            if (start < end && nums[start] == 0) {
+                int temp = start + 1;
+                while (temp <= end) {
+                    nums[temp - 1] = nums[temp];
+                    temp++;
+                }
+                nums[end] = 0;
+            } else {
+                start++;
+            }
+        }
+    }
+
+    /**
+     * @param nums
+     */
+    public void moveZeroes2(int[] nums) {
+        int slow = 0, quick = 0, n = nums.length;
+        while (quick < n) {
+            if (nums[quick] != 0) {
+                nums[slow++] = nums[quick];
+            }
+            quick++;
+        }
+        for (int i = slow; i < n; i++) {
+            nums[i] = 0;
+        }
+    }
+
+}
