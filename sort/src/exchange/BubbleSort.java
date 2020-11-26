b3e9c1367b8eb78f9260edc9860a65f23489237b
qihaodong0309 <qihaodong0309@163.com>
2020-11-26 11:32:52 +0800

    @dev：BubbleSort + QuickSort

---
+++ sort/src/exchange/BubbleSort.java
@@ -0,0 +1,43 @@
+package exchange;
+
+import java.util.Random;
+
+/**
+ * 冒泡排序
+ *
+ * @author qihaodong
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        BubbleSort sort = new BubbleSort();
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++) {
+            a[i] = new Random().nextInt(1000);
+        }
+        sort.sort(a);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    /**
+     * 内层循环比较相邻元素，如果前一个大于后一个就交换
+     * 外层循环每次少遍历一个元素
+     * 每轮循环都会将最大的元素交换到数组末尾
+     *
+     * @param a
+     */
+    private void sort(int[] a) {
+        for (int i = 1; i <= a.length - 1; i++) {
+            for (int j = 0; j < a.length - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int k = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = k;
+                }
+            }
+        }
+    }
+
+}
---
+++ sort/src/exchange/QuickSort.java
@@ -0,0 +1,65 @@
+package exchange;
+
+import java.util.Random;
+
+/**
+ * 快速排序
+ *
+ * @author qihaodong
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        QuickSort sort = new QuickSort();
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++) {
+            a[i] = new Random().nextInt(1000);
+        }
+        sort.echo(a, 0, 99);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    /**
+     * 找出基准点，此时该点左边的元素都小于该点值，该点右边的元素都大于该点值
+     * 分别对左右两个数组进行排序
+     *
+     * @param a
+     * @param left
+     * @param right
+     */
+    private void echo(int[] a, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int index = sort(a, left, right);
+        echo(a, left, index - 1);
+        echo(a, index + 1, right);
+    }
+
+    /**
+     * 取第一个元素为基准点，保证左边都比它小，右边都比它大
+     *
+     * @param a
+     * @param left
+     * @param right
+     * @return
+     */
+    private int sort(int[] a, int left, int right) {
+        int key = a[left];
+        while (left < right) {
+            while (left < right && a[right] >= key) {
+                right--;
+            }
+            a[left] = a[right];
+            while (left < right && a[left] <= key) {
+                left++;
+            }
+            a[right] = a[left];
+        }
+        a[left] = key;
+        return left;
+    }
+
+}
