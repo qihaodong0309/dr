d6115f9ac64f2446acf33f0ccd33c09fff6294e1
qihaodong0309 <qihaodong0309@163.com>
2020-12-09 17:42:46 +0800

    @dev：LeetCode

---
+++ leetCode/src/dp/LeetCode62.java
@@ -0,0 +1,49 @@
+package dp;
+
+/**
+ * @author qihaodong
+ */
+public class LeetCode62 {
+
+    /**
+     * DFS（该方法会超时）
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        return dfs(1, 1, m, n);
+    }
+
+    private int dfs(int x, int y, int m, int n) {
+        if (x == m && y == n) {
+            return 1;
+        }
+        if (x > m || y > n) {
+            return 0;
+        }
+        return dfs(x + 1, y, m, n) + dfs(x, y + 1, m, n);
+    }
+
+    /**
+     * dp
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths2(int m, int n) {
+        int[] record = new int[m + 1];
+        for (int i = 1; i <= m; i++) {
+            record[i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= m; j++) {
+                record[j] += record[j - 1];
+            }
+        }
+        return record[m];
+    }
+
+}
