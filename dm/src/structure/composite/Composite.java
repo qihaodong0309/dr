11d7ff17f9f502a542b35f111cd7daf11ae36634
qihaodong0309 <qihaodong0309@163.com>
2021-03-17 10:37:44 +0800

    @dev：结构类设计模式补充

--- dm/src/structure/composite/Composite.java
+++ dm/src/structure/composite/Composite.java
@@ -1,9 +1,103 @@
 package structure.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 组合模式
  *
  * @author qihaodong
  */
 public class Composite {
+
+    public static void main(String[] args) {
+        Composite test = new Composite();
+        Component c0 = test.new CompositeTest();
+        Component c1 = test.new CompositeTest();
+        Component l1 = test.new Leaf("叶子结点(1)");
+        Component l2 = test.new Leaf("叶子结点(2)");
+        Component l3 = test.new Leaf("叶子结点(3)");
+        c0.add(l1);
+        c0.add(c1);
+        c1.add(l2);
+        c1.add(l3);
+        c0.operation();
+    }
+
+    /**
+     * 抽象构件
+     */
+    interface Component {
+
+        void add(Component c);
+
+        void remove(Component c);
+
+        Component getChild(int i);
+
+        void operation();
+
+    }
+
+    /**
+     * 树叶构件
+     */
+    class Leaf implements Component {
+
+        String name;
+
+        public Leaf(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void add(Component c) {
+
+        }
+
+        @Override
+        public void remove(Component c) {
+
+        }
+
+        @Override
+        public Component getChild(int i) {
+            return null;
+        }
+
+        @Override
+        public void operation() {
+            System.out.println("树叶" + name + "被访问");
+        }
+
+    }
+
+    class CompositeTest implements Component {
+
+        private List<Component> children = new ArrayList<>();
+
+        @Override
+        public void add(Component c) {
+            children.add(c);
+        }
+
+        @Override
+        public void remove(Component c) {
+            children.remove(c);
+        }
+
+        @Override
+        public Component getChild(int i) {
+            return children.get(i);
+        }
+
+        @Override
+        public void operation() {
+            for (Component c : children) {
+                c.operation();
+            }
+        }
+    }
+
+
 }
--- dm/src/structure/flyweight/Flyweight.java
+++ dm/src/structure/flyweight/Flyweight.java
@@ -1,5 +1,8 @@
 package structure.flyweight;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 享元模式
  *
@@ -7,5 +10,62 @@ package structure.flyweight;
  */
 public class Flyweight {
 
+    public static void main(String[] args) {
+        Flyweight test = new Flyweight();
+        FlyweightFactory factory = test.new FlyweightFactory();
+        FlyweightTest flyweight1 = factory.getConFlyweight("x");
+        FlyweightTest flyweight2 = factory.getConFlyweight("xx");
+        FlyweightTest flyweight3 = factory.getConFlyweight("xxx");
+        FlyweightTest flyweight4 = factory.getConFlyweight("xx");
+        FlyweightTest flyweight5 = factory.getConFlyweight("x");
+        flyweight1.operation();
+        flyweight2.operation();
+        flyweight3.operation();
+        flyweight4.operation();
+        flyweight5.operation();
+        System.out.println(flyweight1 == flyweight5);
+        System.out.println(flyweight2 == flyweight4);
+    }
+
+    /**
+     * 抽象享元角色（所有享元对象共享的模块）
+     */
+    interface FlyweightTest {
+        void operation();
+    }
+
+    /**
+     * 具体享元角色
+     */
+    class ConFlyweight implements FlyweightTest {
+
+        String key;
+
+        public ConFlyweight(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public void operation() {
+            System.out.println("享元角色 " + key + " operation() 方法被调用！");
+        }
+    }
+
+    /**
+     * 享元角色工厂
+     */
+    class FlyweightFactory {
+
+        private Map<String, FlyweightTest> map = new HashMap<>();
+
+        private FlyweightTest getConFlyweight(String key) {
+            FlyweightTest result = map.get(key);
+            if (result == null) {
+                result = new ConFlyweight(key);
+                map.put(key, result);
+            }
+            return result;
+        }
+    }
 
 }
