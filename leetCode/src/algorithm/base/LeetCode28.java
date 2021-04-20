d58e066899055da3aca4fe6c7f01253abc3061b1
qihaodong0309 <qihaodong0309@163.com>
2021-04-20 09:40:25 +0800

    @dev：力扣刷题

---
+++ leetCode/src/algorithm/base/LeetCode28.java
@@ -0,0 +1,43 @@
+package algorithm.base;
+
+/**
+ * @author qihaodong
+ */
+public class LeetCode28 {
+
+    /**
+     * 直接使用函数
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    /**
+     * 自己实现函数
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr2(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
